25c08938c6ba4c01ceeffe758be547f24b53b648
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-26 19:13:00 +0200

    api endpoint to register a new user

---
+++ api/routes/auth.js
@@ -0,0 +1,23 @@
+import express from "express";
+import User from "../models/User.js";
+import CryptoJS from "crypto-js";
+
+const router = express.Router();
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
