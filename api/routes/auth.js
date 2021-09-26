6c1186776b5ae8e0e27d1a3bff429b24dbff2534
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-26 19:14:39 +0200

    login api route with jwt implementation

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -21,3 +21,31 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username!");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "30d" }
+    );
+
+    const { password, ...info } = user._doc; //dont send password with response
+
+    res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;
