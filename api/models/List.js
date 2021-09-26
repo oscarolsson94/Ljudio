b904d1d3bda15f8ddba51d1a0a9c55f1c99c2e0a
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-26 20:54:04 +0200

    adding listURL field to list model

--- api/models/List.js
+++ api/models/List.js
@@ -5,6 +5,7 @@ const ListSchema = new mongoose.Schema(
     title: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     content: { type: Array },
+    listURL: { type: String },
   },
   { timestamps: true }
 );
