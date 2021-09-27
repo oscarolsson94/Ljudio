96d1c7223f820dfc01e148d86c5052b03b3ba238
AdrianCo97 <adrian.corazza@outlook.com>
2021-09-27 15:40:01 +0200

    Created the login component.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import { useState } from 'react';
+import Axios from "axios";
+import "../styling/LoginFormStyle.css"
 
 function LoginForm() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const confirmLogin = () => {
+        Axios.post("http://localhost:3001/api/auth/login", {
+            email: email,
+            password: password
+        }).then(() => {
+            console.log("Success!");
+        })
+        
+    }
+    
     return (
-        <div>
-            
-        </div>
-    )
+      <form className="loginForm" onSubmit={confirmLogin}>
+          <p>Email</p>
+          <input type="email" required onChange={(e) => setEmail(e.target.value)}></input>
+
+          <p>Password</p>
+          <input type="password" required onChange={(e) => setPassword(e.target.value)}></input>
+          
+          <hr/>
+
+          <button type="submit">Log in</button>
+      </form>    
+   );
 }
 
 export default LoginForm
