e815a302dc32ad6bfa82f971349d97691133b043
AdrianCo97 <adrian.corazza@outlook.com>
2021-09-27 16:08:45 +0200

    Added RegisterForm to the route

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact component={LoginForm}/>
+        <Route path="/register" component={RegisterForm}/>
       </Switch>
     </Router>
   );
