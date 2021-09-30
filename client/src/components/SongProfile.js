f54c6a99b11933bed7bfbf536c0dc0f426ba6b5a
AdrianCo97 <adrian.corazza@outlook.com>
2021-09-30 10:54:04 +0200

    Created a basic player

--- client/src/components/SongProfile.js
+++ client/src/components/SongProfile.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import YTPlayer from "yt-player";
 
 function SongProfile() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+  let player;
+
+  useEffect(() => {
+    player = new YTPlayer("#ytPlayer")
+
+    player.load("5ys27BJXDD0");
+  }, [])
+
+  const playVideo = () => {
+    player.play();
+  }
+
+  const pauseVideo = () => {
+    player.pause();
+  }
+
+  return (
+    <div>
+      <div id="ytPlayer"></div>
+      <div>
+      
+        <button onClick={playVideo}>Play</button>
+        <button onClick={pauseVideo}>Pause</button>
+      </div>
+    </div>
+  )
 }
 
 export default SongProfile
