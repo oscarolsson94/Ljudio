6ad622a871bde9e0adf341897c17103bb52e7956
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-10-01 18:29:22 +0200

    adding redirect to login when registered

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -45,7 +45,7 @@ router.post("/login", async (req, res) => {
 
     res.status(200).json({ ...info, accessToken });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json("No user with that username and password combination");
   }
 });
 
