80832f1bce39808dff2e88a41dca3c051722a7f5
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-10-01 21:59:08 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,35 @@
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Player from "./components/Player";
+import { UserContext } from "./UserContext";
+
 function App() {
-  return <div className="App">App</div>;
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || {
+      username: "",
+      email: "",
+      token: "",
+      playLists: [],
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+          <Route path="/song=:videoId" component={Player} />
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
