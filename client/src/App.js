7e53887a131278eec65dd1696c902f906dc543eb
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-10-01 18:44:35 +0200

    changes to add song to list backend & axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import { useState, useEffect } from "react";
-import {BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import {AuthContext} from "./AuthContext"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { AuthContext } from "./AuthContext";
 import Player from "./components/Player";
 
 function App() {
-
   const [token, setToken] = useState(null);
-  
+
   useEffect(() => {
-      setToken(localStorage.getItem("myToken"));
-  },[token]);
+    setToken(localStorage.getItem("myToken"));
+  }, [token]);
 
   return (
     <Router>
       <Switch>
-        <AuthContext.Provider value={{token, setToken}}>
-          <Route path="/" exact component={LoginForm}/>
-          <Route path="/register" component={RegisterForm}/>
-          <Route path="/song=:videoId" component={Player}/>
+        <AuthContext.Provider value={{ token, setToken }}>
+          <Route path="/" exact component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+          <Route path="/song=:videoId" component={Player} />
         </AuthContext.Provider>
       </Switch>
     </Router>
--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import YTPlayer from "yt-player";
-import "../styling/PlayerStyle.css"
+import "../styling/PlayerStyle.css";
 
 function Player() {
-
   let { videoId } = useParams();
 
   const [artist, setArtist] = useState();
@@ -16,67 +16,84 @@ function Player() {
   const [intervalId, setIntervalId] = useState(0);
 
   useEffect(() => {
-    const getData = async() => {
-      const response = await fetch("https://yt-music-api.herokuapp.com/api/yt/song/" + videoId)
-    let result = await response.json();
-    console.log(result);
-    setArtist(result.artist.name);
-    setSongName(result.name);
-    setAlbumCover(result.thumbnails[1].url)
-    }
+    const getData = async () => {
+      const response = await fetch(
+        "https://yt-music-api.herokuapp.com/api/yt/song/" + videoId
+      );
+      let result = await response.json();
+      console.log(result);
+      setArtist(result.artist.name);
+      setSongName(result.name);
+      setAlbumCover(result.thumbnails[1].url);
+    };
     getData();
-    
-    let ytPlayer = new YTPlayer("#ytPlayer")
+
+    let ytPlayer = new YTPlayer("#ytPlayer");
     ytPlayer.load(videoId);
-    setPlayer(ytPlayer);   
+    setPlayer(ytPlayer);
   }, [videoId]);
+
+  const handleAddToList = () => {
+    axios.patch(`http://localhost:3001/api/lists/addto/${listTitle}`, {
+      songURL: videoId,
+    });
+  };
 
   const playVideo = () => {
     setDuration(player.getDuration());
     player.play();
     startCount();
-  }
-  
+  };
+
   const pauseVideo = () => {
     player.pause();
     stopCount();
-  }
+  };
 
-  const changeVideoProgress = async(event) => {
+  const changeVideoProgress = async (event) => {
     pauseVideo();
     let newProgressValue = await event.target.value;
     setProgress(newProgressValue);
     player.seek(progress);
-  }
+  };
 
   const startCount = () => {
     const newIntervalId = setInterval(() => {
       setProgress(player.getCurrentTime());
-    }, 1000)
+    }, 1000);
     setIntervalId(newIntervalId);
-  } 
+  };
 
   const stopCount = () => {
-    if(intervalId){
+    if (intervalId) {
       clearInterval(intervalId);
       setIntervalId(0);
       return;
     }
-  }
+  };
   return (
     <div className="body">
       <img src={albumCover} alt="album cover"></img>
       <p>{artist}</p>
       <p>{songName}</p>
       <div id="ytPlayer"></div>
-      <input className="progressBar" type="range" value={progress} onChange={changeVideoProgress} onMouseUp={playVideo} max={duration}/>
+      <input
+        className="progressBar"
+        type="range"
+        value={progress}
+        onChange={changeVideoProgress}
+        onMouseUp={playVideo}
+        max={duration}
+      />
       <div>
-      
         <button onClick={playVideo}>Play</button>
         <button onClick={pauseVideo}>Pause</button>
       </div>
+      <div>
+        <button onClick={handleAddToList}>Add to my list</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Player;
