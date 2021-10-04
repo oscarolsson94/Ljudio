b13c9bf3bd834929ad23d2a3f335d48f0ffc6f56
mirolongo <70956059+mirolongo@users.noreply.github.com>
2021-10-04 11:42:48 +0200

    Created preview image next to song.
    Changed SearchPage.js and SearchPage.css files

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,55 +1,90 @@
 import React, { useState } from 'react'
-import { FaSearch } from 'react-icons/fa';
-import HomeButton from './HomeButton';
-
-import "../css/SearchPage.css"
-
+import { FaSearch } from 'react-icons/fa'
+import HomeButton from './HomeButton'
+import '../css/SearchPage.css'
 function SearchPage() {
-    const [input, setInput] = useState('')
-    const [songs, setSongs] = useState()
-
-    //Consult the youtube api to search for songs 
-    async function searchSong() {
-        let response = await fetch('https://yt-music-api.herokuapp.com/api/yt/songs/' + input)
-        let result = await response.json()
-        setSongs(result.content)
-        console.log(result.content)
-      }
-
-      //Creates a button with the search figure with the search function
-      function SearchButton(){
-        return (
-            <button className="search-button" onClick={searchSong}>
-                <FaSearch size={17} />
-            </button>
-           
-        );
-    };
-    
-    return (
-        <>
-            <div className="container-search-page" >
-                <h3 className="title" >Tillgängliga låter/Artister</h3>
-            </div>
-            <div className="input-search">
-                <input  className="input"
-                    type="text" placeholder="Sök på låt eller artist"
-                     onChange={e => setInput(e.target.value)}>
-                </input>
-                <SearchButton onClick={searchSong}></SearchButton>
-            </div>
-            <div> 
-                <div>
-                    {songs && songs.map(song => (
-                     <div className="list-song" >{song.name}
-                     </div>
-                     ))}
-                </div>
-                <div className="home">
-                     <HomeButton></HomeButton>
-                 </div>
-            </div> 
-        </>
+  const [input, setInput] = useState('')
+  const [songs, setSongs] = useState()
+  const [images, setImages] = useState()
+  //Consult the youtube api to search for songs
+  async function searchSong() {
+    let response = await fetch(
+      'https://yt-music-api.herokuapp.com/api/yt/songs/' + input,
     )
+    let result = await response.json()
+    setSongs(result.content)
+  }
+  async function searchImage() {
+    let response = await fetch(
+      'https://yt-music-api.herokuapp.com/api/yt/songs/' + input,
+    )
+    let result = await response.json()
+    for (let i = 0; i < result.content.length; i++) {
+      console.log(result.content[i])
+      let res = result.content[i]
+      let image = res.thumbnails[0].url
+      //images.push(image)
+      setImages(image)
+      console.log(image)
+    }
+  }
+  //Creates a button with the search figure with the search function
+  function SearchButton() {
+    return (
+      <button
+        className="search-button"
+        onClick={() => {
+          searchSong()
+          searchImage()
+        }}
+      >
+        <FaSearch size={17} />
+      </button>
+    )
+  }
+  return (
+    <>
+      <div className="container-search-page">
+        <h3 className="title">Tillgängliga låter/Artister</h3>
+      </div>
+      <div className="input-search">
+        <input
+          className="input"
+          type="text"
+          placeholder="Sök på låt eller artist"
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+
+        <SearchButton
+          onClick={() => {
+            searchSong()
+            searchImage()
+          }}
+        ></SearchButton>
+      </div>
+      <div>
+        <div>
+          {songs &&
+            songs.map((song) => (
+              <div className="list-song">
+                {song.name}
+                <searchImage>
+                  <div>
+                    {songs &&
+                      songs.map((song) => (
+                        <img src={song.thumbnails[0].url} alt="images" />
+                      ))}
+                  </div>
+                </searchImage>
+              </div>
+            ))}
+        </div>
+
+        <div className="home">
+          <HomeButton></HomeButton>
+        </div>
+      </div>
+    </>
+  )
 }
 export default SearchPage
