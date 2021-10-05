4e0d3bc0e0502c92a2d5c2883346e3031b113287
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-10-05 18:38:57 +0200

    adding title and id to list content

--- api/models/List.js
+++ api/models/List.js
@@ -4,7 +4,7 @@ const ListSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
     username: { type: String, required: true },
-    content: { type: Array, default: [] },
+    content: { type: [{ title: String, id: String }], default: [] },
     listURL: { type: String },
   },
   { timestamps: true }
