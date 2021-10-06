c49a4d8c79870ab4db212786c08354b338bc2aee
AdrianCo97 <adrian.corazza@outlook.com>
2021-10-06 12:18:13 +0200

    Added the PlayerContext provider

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,10 @@ import Playlists from "./components/Playlists";
 import NewPlaylist from "./components/NewPlaylist";
 import PlaylistPage from "./components/PlaylistPage";
 import SearchPage from "./components/SearchPage";
+import { PlayerContext } from "./contexts/PlayerContext";
 
 function App() {
+  const [queue, setQueue] = useState([]);
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("user")) || {
       username: "",
@@ -25,6 +27,7 @@ function App() {
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
+    <PlayerContext.Provider value={{queue, setQueue}}>
       <Router>
         <Switch>
           <Route path="/" exact component={LoginForm} />
@@ -36,6 +39,7 @@ function App() {
           <Route path="/playlist=:title" component={PlaylistPage} />
         </Switch>
       </Router>
+    </PlayerContext.Provider>
     </UserContext.Provider>
   );
 }
