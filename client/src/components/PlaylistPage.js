9ea97d683928eb818e80ad0b1be0c36152bdcf95
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-10-08 15:20:44 +0200

    moving icon to the correct page..

--- client/src/components/PlaylistPage.js
+++ client/src/components/PlaylistPage.js
@@ -2,14 +2,15 @@ import axios from "axios";
 import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router";
 import { PlayerContext } from "../contexts/PlayerContext";
-import PlayCircleFilledOutlinedIcon from '@mui/icons-material/PlayCircleFilledOutlined';
-import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import PlayCircleFilledOutlinedIcon from "@mui/icons-material/PlayCircleFilledOutlined";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
 import { useHistory } from "react-router";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "../styling/PlaylistPageStyle.css";
 
 function PlaylistPage() {
   const { title } = useParams();
-  const { queue, setQueue } = useContext(PlayerContext)
+  const { queue, setQueue } = useContext(PlayerContext);
   const [playlist, setPlaylist] = useState([]);
 
   const history = useHistory();
@@ -20,53 +21,62 @@ function PlaylistPage() {
         "http://localhost:3001/api/lists/single/" + title
       );
       setPlaylist(result.data.content);
-      setQueue({...queue, queueList: result.data.content});
+      setQueue({ ...queue, queueList: result.data.content });
     };
     getAllPlaylists();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-
   const playPlaylist = () => {
     history.push("/song=" + playlist[0].songId);
-  }
+  };
 
   const playSong = (song, index) => {
     history.push("/song=" + song.songId);
-    setQueue({...queue, queueIndex: index})
+    setQueue({ ...queue, queueIndex: index });
     console.log(queue.queueIndex);
-  }
+  };
 
   const goBack = () => {
     history.push("/playlists");
-  }
+  };
+
+  const handleDelete = () => {
+    console.log("delete");
+  };
 
   return (
     <div className="site">
       <div className="header">
-        <ArrowBackIosNewOutlinedIcon onClick={goBack} fontSize="large"/>
+        <ArrowBackIosNewOutlinedIcon onClick={goBack} fontSize="large" />
         <h1>{title}</h1>
-        <PlayCircleFilledOutlinedIcon color="action" fontSize="large" onClick={playPlaylist}/>
+        <PlayCircleFilledOutlinedIcon
+          color="action"
+          fontSize="large"
+          onClick={playPlaylist}
+        />
       </div>
 
       <div className="listContent">
         {playlist.map((song, index) => (
-          <div className="songBody" key={song._id} onClick={() => {playSong(song, index)}}>
-            <img src={song.coverPic} alt="album" /> 
+          <div
+            className="songBody"
+            key={song._id}
+            onClick={() => {
+              playSong(song, index);
+            }}
+          >
+            <img src={song.coverPic} alt="album" />
             <div className="textContent">
               <p>{song.artist}</p>
               <p>{song.title}</p>
             </div>
+            <DeleteIcon onClick={handleDelete} />
           </div>
-      ))}
-
+        ))}
       </div>
-      
-      
     </div>
   );
-  
-
 }
 
 export default PlaylistPage;
