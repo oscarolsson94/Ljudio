e81dee1e8079d3193a94b73c56186bbc4ff6cb2f
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-10-10 15:53:01 +0200

    resolving weird bug causing main to lost commits

---
+++ api/routes/lists.js
@@ -0,0 +1,78 @@
+import express from "express";
+import List from "../models/List.js";
+import verify from "../verifyToken.js";
+
+const router = express.Router();
+
+//GET ONE LIST BY TITLE
+router.get("/single/:id", async (req, res) => {
+  try {
+    const userList = await List.findOne({ title: req.params.id });
+    res.send(userList);
+  } catch (error) {
+    res.status(404).json(err);
+  }
+});
+
+//GET ALL LISTS BY USERNAME - id = username
+router.get("/:id", async (req, res) => {
+  try {
+    const userLists = await List.find({ username: req.params.id });
+    res.send(userLists);
+  } catch (error) {
+    res.status(404).json(err);
+  }
+});
+
+//CREATE body{title, username, }
+router.post("/", verify, async (req, res) => {
+  const newList = new List(req.body);
+  try {
+    const savedList = await newList.save();
+    res.status(201).json(savedList);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verify, async (req, res) => {
+  try {
+    await List.deleteOne({ title: req.params.id });
+    res.status(204).json("The list has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//ADD SONG TO LIST id = listTitle - send PATCH request with body{title, songId, artist, coverPic }
+router.patch("/addto/:id", verify, async (req, res) => {
+  try {
+    await List.updateOne(
+      { title: req.params.id },
+      {
+        $push: { content: req.body },
+      }
+    );
+    res.status(201).json("The song has been added to the list");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//REMOVE SONG FROM LIST id = title - send PATCH request with body{songId}
+router.patch("/removefrom/:id", verify, async (req, res) => {
+  try {
+    await List.updateOne(
+      { title: req.params.id },
+      {
+        $pull: { content: { songId: req.body.songId } },
+      }
+    );
+    res.status(204).json("The song has been removed from the list");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;
---
+++ api/verifyToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export default function verify(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+}
