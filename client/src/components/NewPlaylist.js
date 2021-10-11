f1034c88dc260284a13fbd4e60fb13a4a0235fa7
mirolongo <70956059+mirolongo@users.noreply.github.com>
2021-10-11 09:04:42 +0200

    Made the style of the newPlaylist page
    Created the NewPlaylist.css file and inserted the classes in NewPlaylist.js

--- client/src/components/NewPlaylist.js
+++ client/src/components/NewPlaylist.js
@@ -1,44 +1,57 @@
-import React, { useState, useContext } from "react";
-import axios from "axios";
-import { UserContext } from "../contexts/UserContext";
-import { Redirect, useHistory } from "react-router";
+import React, { useState, useContext } from 'react'
+import axios from 'axios'
+import { UserContext } from '../contexts/UserContext'
+import { Redirect, useHistory } from 'react-router'
+import '../styling/NewPlaylist.css'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 function NewPlaylist() {
-  const [title, setTitle] = useState("");
-  const { user } = useContext(UserContext);
-  const history = useHistory();
+  const [title, setTitle] = useState('')
+  const { user } = useContext(UserContext)
+  const history = useHistory()
 
   const createPlayList = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     axios
       .post(
-        "http://localhost:3001/api/lists/",
+        'http://localhost:3001/api/lists/',
         {
           username: user.username,
           title: title,
         },
         {
-          headers: { Authorization: "Bearer " + user.token },
-        }
+          headers: { Authorization: 'Bearer ' + user.token },
+        },
       )
       .then(() => {
-        history.push("/playlists");
-      });
-  };
+        history.push('/playlists')
+      })
+  }
 
-  if (!user.token) return <Redirect to="/" />;
+  if (!user.token) return <Redirect to="/" />
   return (
     <div>
-      <p>Create a new Playlist</p>
-
+      <p className="title">Skapa en ny Spellista</p>
+      <ArrowBackIosIcon
+        className="button-back"
+        sx={{ fontSize: 35 }}
+        onClick={() => {
+          history.push('/playlists')
+        }}
+      ></ArrowBackIosIcon>
+      <hr/>
       <form onSubmit={createPlayList}>
-        <input type="text" onChange={(e) => setTitle(e.target.value)}></input>
+      <div className="form">
+        <input type="text" className="input"
+         placeholder="Namn på ny Spellista"
+        onChange={(e) => setTitle(e.target.value)}></input>
 
-        <button type="submit" value="Name of the playlist">
-          Add playlist
+        <button type="submit" className="button-add" value="Name of the playlist">
+        Lägg till Spellista
         </button>
+        </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default NewPlaylist;
+export default NewPlaylist
